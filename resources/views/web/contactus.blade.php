2a4d62ebbbbb1b30483dec652e333b906a890ea2
Mradul Sharma <15956546+ermradulsharma@users.noreply.github.com>
2025-09-27 22:27:53 +0530

    contect us

--- resources/views/web/contactus.blade.php
+++ resources/views/web/contactus.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.web.web')
+
 @section('content')
+
 <!-- Full Screen Search Start -->
 <div class="modal fade" id="searchModal" tabindex="-1">
     <div class="modal-dialog modal-fullscreen">
@@ -25,6 +27,7 @@
             <h5 class="fw-bold text-primary text-uppercase">Contact Us</h5>
             <h1 class="mb-0">If You Have Any Query, Feel Free To Contact Us</h1>
         </div>
+
         <div class="row g-5 mb-5">
             <div class="col-lg-4">
                 <div class="d-flex align-items-center wow fadeIn" data-wow-delay="0.1s">
@@ -33,7 +36,7 @@
                     </div>
                     <div class="ps-4">
                         <h5 class="mb-2">Call to ask any question</h5>
-                        <h3 class="text-primary mb-0">+91-8273226886</h3>
+                        <h3 class="text-primary mb-0">+91 7252933077</h3>
                     </div>
                 </div>
             </div>
@@ -44,7 +47,7 @@
                     </div>
                     <div class="ps-4">
                         <h5 class="mb-2">Email to get free quote</h5>
-                        <p class="text-primary mb-0">zaibainfotech2019@gmail.com</p>
+                        <p class="text-primary mb-0">info@skywalkerintelligence.com</p>
                     </div>
                 </div>
             </div>
@@ -55,27 +58,29 @@
                     </div>
                     <div class="ps-4">
                         <h5 class="mb-2">Visit our office</h5>
-                        <h6 class="text-primary mb-0">
- Agra, Uttar Pradesh 282001</h6>
+                        <h6 class="text-primary mb-0">Agra, Uttar Pradesh 282001</h6>
                     </div>
                 </div>
             </div>
         </div>
+
         <div class="row g-5">
+            <!-- Contact Form -->
             <div class="col-lg-6 wow slideInUp" data-wow-delay="0.3s">
-                <form>
+                <form action="#" method="POST">
+                    @csrf
                     <div class="row g-3">
                         <div class="col-md-6">
-                            <input type="text" class="form-control border-0 bg-light px-4" placeholder="Your Name" style="height: 55px;">
+                            <input type="text" name="name" class="form-control border-0 bg-light px-4" placeholder="Your Name" style="height: 55px;" required>
                         </div>
                         <div class="col-md-6">
-                            <input type="email" class="form-control border-0 bg-light px-4" placeholder="Your Email" style="height: 55px;">
+                            <input type="email" name="email" class="form-control border-0 bg-light px-4" placeholder="Your Email" style="height: 55px;" required>
                         </div>
                         <div class="col-12">
-                            <input type="text" class="form-control border-0 bg-light px-4" placeholder="Subject" style="height: 55px;">
+                            <input type="text" name="subject" class="form-control border-0 bg-light px-4" placeholder="Subject" style="height: 55px;" required>
                         </div>
                         <div class="col-12">
-                            <textarea class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message"></textarea>
+                            <textarea name="message" class="form-control border-0 bg-light px-4 py-3" rows="4" placeholder="Message" required></textarea>
                         </div>
                         <div class="col-12">
                             <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
@@ -83,6 +88,8 @@
                     </div>
                 </form>
             </div>
+
+            <!-- Google Map -->
             <div class="col-lg-6 wow slideInUp" data-wow-delay="0.6s">
                 <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
@@ -96,15 +103,15 @@
     <div class="container py-5 mb-5">
         <div class="bg-white">
             <div class="owl-carousel vendor-carousel">
-                <img src="img/vendor-1.jpg" alt="">
-                <img src="img/vendor-2.jpg" alt="">
-                <img src="img/vendor-3.jpg" alt="">
-                <img src="img/vendor-4.jpg" alt="">
-                <img src="img/vendor-5.jpg" alt="">
-                <img src="img/vendor-6.jpg" alt="">
-                <img src="img/vendor-7.jpg" alt="">
-                <img src="img/vendor-8.jpg" alt="">
-                <img src="img/vendor-9.jpg" alt="">
+                <img src="{{ asset('web/img/vendor-1.jpg') }}" alt="Vendor 1">
+                <img src="{{ asset('web/img/vendor-2.jpg') }}" alt="Vendor 2">
+                <img src="{{ asset('web/img/vendor-3.jpg') }}" alt="Vendor 3">
+                <img src="{{ asset('web/img/vendor-4.jpg') }}" alt="Vendor 4">
+                <img src="{{ asset('web/img/vendor-5.jpg') }}" alt="Vendor 5">
+                <img src="{{ asset('web/img/vendor-6.jpg') }}" alt="Vendor 6">
+                <img src="{{ asset('web/img/vendor-7.jpg') }}" alt="Vendor 7">
+                <img src="{{ asset('web/img/vendor-8.jpg') }}" alt="Vendor 8">
+                <img src="{{ asset('web/img/vendor-9.jpg') }}" alt="Vendor 9">
             </div>
         </div>
     </div>
